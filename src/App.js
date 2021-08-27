ba39af91bd3a41fc00c49ef9eb2291f35ddb2121
jigz <568919463@qq.com>
2021-08-27 10:50:26 +0800

    入口函数,根组件和项目基本结构搭建

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+应用的根组件  
+*/
+import React , { Component } from 'react'
+export default class App extends Component{
+render(){//使用jsx语法书写虚拟dom
+    return <dev>HelloWorld</dev>
+    }
 }
-
-export default App;
